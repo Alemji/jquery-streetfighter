d6d1ba5da736e444bbb18082acc2c1cdf7a69725
Alemji <Alemji@users.noreply.github.com>
2015-08-15 15:52:37 -0400

    final

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() { 
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		
 	})
 
 	$('.ryu').mouseleave(function() {
@@ -10,22 +11,55 @@ $(document).ready(function() {
 	})
 
 	$('.ryu').mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate({left: '1020px'}, 500,
+		function() {
+			$(this).hide();
+			$(this).css('left', '520px');
+			}) /* this anonymous function will be called at the completion
+		of the animation to reset the hadouken.*/
 		//play hadouken sound
-		//animate it to the right of the screen
-
 	})
 
 	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+		$('.ryu-still').hide();
 		//ryu goes back to his ready position
 	})
 
 
+	$('body').keydown(function(e){
+		if(e.keyCode == 88){
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('ryu-throwing').hide();
+			$('.ryu-cool').show()}
+		}) 
+
+	$('body').keyup(function(e){
+		if(e.keyCode == 88){
+			$('.ryu-cool').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-still').show();}
+		}) // I don't understand why some functions need something in ()...
+	
+	$('.how-to')//.css('left','300px').css('top', '150px');
+	
+	
 });
+
+function playHadouken () {
+	$('#hadouken-sound') [0].volume = 0.5;
+	$('#hadouken-sound') [0].load();
+	$('#hadouken-sound') [0].play();
+}
+
+
+
+
 
 
 
